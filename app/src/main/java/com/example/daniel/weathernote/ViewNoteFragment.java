1170ab47051dd2188b67d00f6c1a8bfe0a7487be
Daniel Song <daniel100333@yahoo.com>
2016-04-01 07:18:22 -0400

    Started adding class for viewing an already created note

---
+++ app/src/main/java/com/example/daniel/weathernote/ViewNoteFragment.java
@@ -0,0 +1,11 @@
+package com.example.daniel.weathernote;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Daniel on 4/1/2016.
+ */
+public class ViewNoteFragment extends Fragment {
+
+    private static final String TAG = ViewNoteFragment.class.toString();
+}
