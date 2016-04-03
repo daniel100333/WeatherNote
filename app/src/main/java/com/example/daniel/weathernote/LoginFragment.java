be85a678d2566579ad527a886a3d4b3a4db88363
Daniel Song <daniel100333@yahoo.com>
2016-04-02 20:50:52 -0400

    Large UI changes, most notably to the login screen.

--- app/src/main/java/com/example/daniel/weathernote/LoginFragment.java
+++ app/src/main/java/com/example/daniel/weathernote/LoginFragment.java
@@ -2,6 +2,7 @@ package com.example.daniel.weathernote;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextUtils;
@@ -12,12 +13,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by Daniel on 3/4/2016.
  */
 public class LoginFragment extends Fragment {
 
+    private TextInputLayout mEmailTextInputLayout;
     private EditText mEmailField;
     private Button mLoginButton;
 
@@ -32,45 +35,38 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_login, container, false);
 
-        mEmailField = (EditText) v.findViewById(R.id.login_email);
-        mEmailField.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        mEmailTextInputLayout = (TextInputLayout) v.findViewById(R.id.email_text_input_layout);
+        mEmailField = (EditText) v.findViewById(R.id.input_email);
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (isValidEmail(s)) {
-                    mLoginButton.setVisibility(View.VISIBLE);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        mLoginButton = (Button) v.findViewById(R.id.login);
-        mLoginButton.setVisibility(View.GONE);
+        mLoginButton = (Button) v.findViewById(R.id.btn_login);
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), NoteListActivity.class);
-                startActivity(intent);
+                if (!validate()) {
+                    Toast.makeText(getActivity(), "Login failed", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent intent = new Intent(getActivity(), NoteListActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
         return v;
     }
 
-    public final static boolean isValidEmail(CharSequence target) {
-        if (TextUtils.isEmpty(target)) {
-            return false;
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmailTextInputLayout.setError("enter a valid email address");
+            valid = false;
         } else {
-            return Patterns.EMAIL_ADDRESS.matcher(target).matches();
+            mEmailTextInputLayout.setError(null);
         }
+
+        return valid;
     }
 
 }
